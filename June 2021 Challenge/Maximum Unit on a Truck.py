05e6884320ebf195a7003c7b9d96f115f6b4a0a5
Prashanth Vaidya <prashanthvaidya@Prashanths-MacBook-Air.local>
2021-06-15 06:55:53 +0530

    knapsack greedy

---
+++ June 2021 Challenge/Maximum Unit on a Truck.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/604/week-2-june-8th-june-14th/3778/
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        #knapsack greedy like
+        #sort by units (second idx)
+        boxTypes=sorted(boxTypes,key=lambda x:x[1],reverse=True)
+        ans=0
+        print(boxTypes)
+        for w,val in boxTypes:
+            if(truckSize==0):
+                break
+            taken_wt=min(truckSize,w)
+            ans+=(taken_wt)*val
+            truckSize-=taken_wt
+        
+            
+        
+        return ans
--- June 2021 Challenge/Palindrome Pairs.py
+++ June 2021 Challenge/Palindrome Pairs.py
@@ -1,6 +1,15 @@
 #https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/604/week-2-june-8th-june-14th/3777/
 
-class Solution:    
+class Solution: 
+    def isPali(self,word):
+        i=0
+        j=len(word)-1
+        while i<j:
+            if(word[i]!=word[j]):
+                return False
+            i+=1
+            j-=1
+        return True
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         n=len(words)
         palindromes=set()
@@ -15,25 +24,25 @@ class Solution:
                         palindromes.add((i,r_i))
 
                 #forward
-                first=word[0]
-                for j in range(1,len(word)+1):
-                    second=word[j:]
+                first=""
+                for j in range(len(word)):
+                    first+=word[j]
+                    second=word[j+1:] #also handles the empty string edge case! 
                     rev_first=first[::-1]
                     rev_second=second[::-1]
-                    if(rev_second in lookup  and  first==first[::-1]):
+                    if(rev_second in lookup  and  self.isPali(first)):
                         l_i=lookup[rev_second]
                         if(l_i!=i):
                             palindromes.add((l_i,i))
                             if(rev_second==""): #edge case!
                                 palindromes.add((i,l_i))
-                    if(rev_first in lookup  and  second==second[::-1]):
+                    if(rev_first in lookup  and  self.isPali(second)):
                         l_i=lookup[rev_first]
                         if(l_i!=i):
                             palindromes.add((i,l_i))
                             if(rev_first==""): #edge case!
                                 palindromes.add((i,l_i))
-                    if(j<len(word)):
-                        first+=word[j]
+                    
                 
               
                 
