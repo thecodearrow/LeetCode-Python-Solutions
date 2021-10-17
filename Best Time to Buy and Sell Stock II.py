0f40c304487484bb7a4b7b515c5caa7617384f3a
Prashanth Vaidya <vaiprash97@gmail.com>
2021-10-17 16:07:45 +0530

    Create Best Time to Buy and Sell Stock II.py

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,38 @@
+class Solution:
+   
+    def getProfitDP(self,i,canBuy,n,prices,dp):
+        #Approach1 
+        #DP Solution! 
+        if(i>=n):
+            return 0
+        
+        key=(i,canBuy)
+        if(key in dp):
+            return dp[key]
+        if(canBuy):
+            #buy
+            choice1=-prices[i]+self.getProfitDP(i+1,False,n,prices,dp)
+        else:
+            #sell
+            choice1=prices[i]+self.getProfitDP(i+1,True,n,prices,dp)
+        
+        #do nothing/hold
+        choice2=self.getProfitDP(i+1,canBuy,n,prices,dp)
+        print(i,choice1,choice2)
+        dp[key]=max(choice1,choice2)
+        return dp[key]
+        
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        if(n==1):
+            return 0
+        
+        #Approach 2 optimal (greedy)
+        total_profit=0
+        for i in range(1,n):
+            profit=prices[i]-prices[i-1]
+            total_profit+=max(0,profit)
+        
+        return total_profit
+        
+        
