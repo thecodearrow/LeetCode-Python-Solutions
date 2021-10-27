92b81a08919c221d97a468daab8b5c85d451b87c
Prashanth Vaidya <vaiprash97@gmail.com>
2021-10-27 20:52:20 +0530

    Update Strobogrammatic Number.py

--- Strobogrammatic Number.py
+++ Strobogrammatic Number.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotate(self,num,strobo):
         rotated=[]
-        for c in num:
+        for c in reversed(num):
             if(not strobo[c]):
                 return False,""
             rotated.append(strobo[c])
@@ -10,4 +10,4 @@ class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
         strobo={'0':'0','1':'1','2':False,'3':False,'4':False,'5':False,'6':'9','7':False,'8':'8','9':'6'}
         status,rotated=self.rotate(num,strobo)
-        return status and rotated==num[::-1]
+        return status and rotated==num
