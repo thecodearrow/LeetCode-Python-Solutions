9632bda95f15c59a5027ce1eac413b88c733a8b9
Prashanth Vaidya <vaiprash97@gmail.com>
2021-10-27 15:33:51 +0530

    Create Strobogrammatic Number.py

---
+++ Strobogrammatic Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self,num,strobo):
+        rotated=[]
+        for c in num:
+            if(not strobo[c]):
+                return False,""
+            rotated.append(strobo[c])
+        rotated="".join(rotated)
+        return True,rotated
+    def isStrobogrammatic(self, num: str) -> bool:
+        strobo={'0':'0','1':'1','2':False,'3':False,'4':False,'5':False,'6':'9','7':False,'8':'8','9':'6'}
+        status,rotated=self.rotate(num,strobo)
+        return status and rotated==num[::-1]
