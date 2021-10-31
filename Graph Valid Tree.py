7afed7288aeae26fc3ad69a94bed0512c7a3bf66
Prashanth Vaidya <vaiprash97@gmail.com>
2021-10-31 05:31:52 +0530

    Update Graph Valid Tree.py

--- Graph Valid Tree.py
+++ Graph Valid Tree.py
@@ -1,3 +1,9 @@
+'''
+You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+
+Return true if the edges of the given graph make up a valid tree, and false otherwis
+'''
+
 class DisjointSet:
     def __init__(self,n):
         self.parent=[i for i in range(n)]
