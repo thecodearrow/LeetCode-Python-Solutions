9f2cd4ef6ed3bdf7874cd2ee3e5eabb1bfe2dc87
Prashanth Vaidya <vaiprash97@gmail.com>
2021-11-02 20:51:43 +0530

    Create Path With Minimum Effort.py

---
+++ Path With Minimum Effort.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        #modified dijkstra! 
+        heap=[(0,0,0)] #diff,i,j
+        visited=set()
+        visited.add((0,0))
+        n=len(heights)
+        m=len(heights[0])
+        diff=defaultdict(lambda: float("inf"))
+        max_effort=0 #minimise max effort on path! 
+        while heap:
+            effort,i,j=heapq.heappop(heap) #the effort here will be minimum possible effort to get here!
+            max_effort=max(max_effort,effort)
+            dirs=[(0,1),(1,0),(0,-1),(-1,0)]
+            if(i==n-1 and j==m-1):
+                return max_effort
+            for dx,dy in dirs:
+                x=i+dx
+                y=j+dy
+                if(x>=0 and y>=0 and x<n and y<m):
+                    if((x,y) not in visited):
+                        current_effort=abs(heights[x][y]-heights[i][j])
+                        if(current_effort<diff[(x,y)]):
+                            diff[(x,y)]=current_effort
+                            heapq.heappush(heap,(current_effort,x,y))
+        
+        
+                        
+                
+                
